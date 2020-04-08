0d160ce02e496d91468cdc0c5c2a1233074bc86f
Maciej Gruszczyński <maciej.gruszczynski@gmail.com>
2020-04-08 23:55:58 +0200

    bootstrap layout added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,8 +4,15 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("@rails/activestorage").start()
+require("turbolinks").start()
 
+import "bootstrap"
+import "../stylesheets/application"
+
+document.addEventListener("turbolinks:load", () => {
+  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="popover"]').popover()
+})
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
